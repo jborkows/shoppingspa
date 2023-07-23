56baf408ef68db658b40a13c7c19b69bf80e513c
jborkows <jacek.borkowskiit@gmail.com>
2023-07-23 23:25:24 +0200

    Change scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,4 @@ pnpm install
 
 # npx firebase --project=fakeproject emulators:exec 'npm run test'
  firebase --project=fakeproject --only firestore,auth,hosting emulators:exec 'pnpm run test:firestore'
- pnpm run build
- firebase --project=fakeproject --only firestore,auth,hosting emulators:exec 'pnpm exec playwright test'
+
